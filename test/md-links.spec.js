ef22304dc3ad7dc2745cb4ac3b372b77d921fa0f
Jocelyn <129541722+JoceChile@users.noreply.github.com>
2023-07-19 17:51:07 -0400

    primer test md-Links

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,10 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../index.js');
 
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+  // it('should...', () => {
+  //   console.log('FIX ME!');
+  // });
+  it('deberia devolver una promesa', () => {
+    expect(mdLinks()).toBe(typeof Promise);
   });
-
 });
